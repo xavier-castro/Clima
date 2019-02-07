bdea5a8871759229c62f215cf43cef6874f55844
xavier-castro <xavier_castro@icloud.com>
2019-02-06 18:47:02 -0600

    Ask user for GPS location permissions integrating openweatherapp

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -17,7 +18,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,7 +32,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
+        locationManager.delegate = self // self meaning our current class (WeatherViewController)
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters // Good range that doesn't use that much battery
+        locationManager.requestWhenInUseAuthorization() // Triggers the authorization popup
         
         
     }
