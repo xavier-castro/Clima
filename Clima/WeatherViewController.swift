e0bb6f4269575601193822ab3d2a921aa989bd0b
xavier-castro <xavier_castro@icloud.com>
2019-02-07 21:08:19 -0600

    Added working delegate to send information back

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -153,7 +153,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     //Write the userEnteredANewCityName Delegate method here:
     func userEnteredANewCityName(city: String) {
-        print(city)
+        let params: [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
     }
     
     //Write the PrepareForSegue Method here
